0aaa55b45a5743c722be05acb99c4868aa583449
molekag <48838902+molekag@users.noreply.github.com>
2019-09-03 12:53:28 +0100

    AppointerContext

---
+++ AppointerContext.cs
@@ -0,0 +1,18 @@
+﻿using Appointer.Models;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Appointer
+{
+    public class AppointerContext:DbContext
+    {
+        public AppointerContext():base("")
+        //nie mam webconfig
+        { }
+        public DbSet<Clinic> Clinics { get; set; }
+
+    }
+}
